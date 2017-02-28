ee0a5ced3bc0c41dae70731e758d886f45d9d907
mticciati <matthewticciati@gmail.com>
2017-02-27 16:39:14 -0800

    navigation partial, devise migration first name and last name, full name method on user model

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213061450) do
+ActiveRecord::Schema.define(version: 20170227235026) do
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
@@ -41,9 +40,10 @@ ActiveRecord::Schema.define(version: 20170213061450) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
