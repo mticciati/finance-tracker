bbdb14a34bbe3e86d427cefba2464fadeb205b6f
mticciati <matthewticciati@gmail.com>
2017-03-03 16:05:16 -0800

    stock to stock_id in add stock params

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -25,15 +25,15 @@ class UserStocksController < ApplicationController
   # POST /user_stocks.json
   def create
     if params[:stock_id].present?
-      @user_stock = UserStock.new(stock: params[:stock_id], user: current_user)
+      @user_stock = UserStock.new(stock_id: params[:stock_id], user: current_user)
     else
       stock = Stock.find_by_ticker(params[:stock_ticker])
       if stock
-        @user_stock = UserStock.new(user: current_user, stock: stock)
+        @user_stock = UserStock.new(user: current_user, stock_id: stock)
       else
         stock = Stock.new_from_lookup(params[:stock_ticker])
         if stock.save
-          @user_stock = UserStock.new(user: current_user, stock: stock)
+          @user_stock = UserStock.new(user: current_user, stock_id: stock)
         else
           @user_stocks = nil
           flash[:error] = "Stock is not available"
